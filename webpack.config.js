53c2f69dabef9e6cee0a449616e3f7f88249e950
alex <alexb7217@gmail.com>
2021-03-01 11:38:54 -0800

    wow it works

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: [
     './main/src/index.js',
-    './main/src/index.css'
+    './main/src/index.scss'
   ],
   plugins: [
     new CleanWebpackPlugin(),  // removes outdated assets from the output dir
     new WebpackManifestPlugin(),  // generates the required manifest.json file
+    new MiniCssExtractPlugin(),
   ],
   output: {
     publicPath: '',
@@ -19,19 +21,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/, //regex for css files
+        test: /\.scss$/, //regex for css files
         use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
           {
-            loader: "style-loader" //injects CSS into DOM via <link> tag
-          },
-          {
-            loader: "css-loader", //parse and transform CSS file into modules (opt follows)
+            // Check https://webpack.js.org/loaders/postcss-loader/#plugins for more details
+            loader: "postcss-loader",
             options: {
-              modules: true,
-              importLoaders: 1,
-              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
             }
-          }
+          },
+          { loader: "sass-loader" } // compiles Sass to CSS
         ]
       }
     ]
